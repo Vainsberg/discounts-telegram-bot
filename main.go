83b90cc0799c159cb00cbf480aad073944360b11
Stas Vasilyev <vasilyev@iMac-Stas.local>
2023-11-26 02:53:35 +0300

    Create handler

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type RequestDiscounts struct {
+	Items struct {
+		Name      string `json:"name"`
+		Price_rur int    `json:"price_rur"`
+		Url       string `json:"url"`
+		Image     string `json:"image"`
+	} `json:"items"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var RequestDisc RequestDiscounts
+
+	query := r.URL.Query()
+	queryText := query.Get("query")
+	if queryText == "" {
+
+		fmt.Println(http.StatusBadRequest, w)
+	}
+
+	ApiPlatiRu := "https://plati.io/api/search.ashx?query=" + queryText
+	ApiPlatiRuResp, err := http.Get(ApiPlatiRu)
+	if err != nil {
+
+		fmt.Println(http.StatusBadRequest, w)
+		fmt.Println(err)
+
+	}
+
+	resp, err := io.ReadAll(ApiPlatiRuResp.Body)
+	if err != nil {
+
+		fmt.Println(http.StatusBadRequest, w)
+		fmt.Println(err)
+
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(resp, &RequestDisc)
+	if err != nil {
+
+		fmt.Println(http.StatusBadRequest, w)
+		fmt.Println(err)
+
+	}
+	//responseText := fmt.Sprintf("Игра: %s, цена: %d , cсылка: %s, %s ", RequestDisc.Items.Name, RequestDisc.Items.Price_rur, RequestDisc.Items.Url, RequestDisc.Items.Image)
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/discount", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
